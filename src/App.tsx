0c76169c3345c959f9909214f5eba3a9555805ac
Tales Garcia <garcia.tales@gmail.com>
2020-12-13 20:58:10 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App: React.FC = () => {
+    return (
+        <View>
+            <Text>Hello world</Text>
+        </View>
+    );
+};
+
+export default App;
