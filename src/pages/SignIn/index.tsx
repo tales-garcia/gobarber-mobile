1fd28702965606fbc89a17c0ed794117aa7f0ac6
Tales Garcia <garcia.tales@gmail.com>
2020-12-18 14:06:02 -0300

    feat(signin page): create simple form in SignIn page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import { Image } from 'react-native';
 import { Container, Title } from './styles';
 import logo from '../../assets/images/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => {
     return (
         <Container>
             <Image source={logo} />
             <Title>Faça seu logon</Title>
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button>Entrar</Button>
         </Container>
     );
 }
