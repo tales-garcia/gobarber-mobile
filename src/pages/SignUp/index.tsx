74fc452ca62078c107fe7c25a967426592d985cd
Tales Garcia <garcia.tales@gmail.com>
2020-12-18 16:51:48 -0300

    feat(signup page): create sign up page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,46 @@
 import React from 'react';
-import { Container } from './styles';
+import { Image, KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native';
+import { Container, ForgotPassword, ForgotPasswordText, BackToSignIn, BackToSignInText, Title } from './styles';
+import logo from '../../assets/images/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import Icon from 'react-native-vector-icons/Feather';
 
 const SignUp: React.FC = () => {
     return (
-        <Container />
+        <>
+            <KeyboardAvoidingView
+                style={{ flex: 1 }}
+                behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+                enabled
+            >
+                <ScrollView
+                    keyboardShouldPersistTaps="handled"
+                    contentContainerStyle={{ flex: 1 }}
+                >
+                    <Container>
+                        <Image source={logo} />
+
+                        <View>
+                            <Title>Crie sua conta</Title>
+                        </View>
+
+                        <Input name="name" icon="user" placeholder="Nome" />
+                        <Input name="email" icon="mail" placeholder="E-mail" />
+                        <Input name="password" icon="lock" placeholder="Senha" />
+
+                        <Button>Cadastrar</Button>
+
+                    </Container>
+                </ScrollView>
+            </KeyboardAvoidingView>
+            <BackToSignIn>
+                <Icon color="#fff" size={20} name="arrow-left" />
+                <BackToSignInText>
+                    Voltar para logon
+                </BackToSignInText>
+            </BackToSignIn>
+        </>
     );
 }
 
