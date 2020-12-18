7b125323a036377b2b98418ef5efb2b4a3e34239
Tales Garcia <garcia.tales@gmail.com>
2020-12-18 15:19:01 -0300

    feat(signin page): surround container with keyboard avoinding view
    Now the page adapts itself according to the keyboar

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -9,26 +9,31 @@ import Icon from 'react-native-vector-icons/Feather';
 const SignIn: React.FC = () => {
     return (
         <>
-            <Container>
-                <Image source={logo} />
+            <KeyboardAvoidingView
+                style={{ flex: 1 }}
+                behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+                enabled
+            >
+                <Container>
+                    <Image source={logo} />
 
-                <View>
-                    <Title>Faça seu logon</Title>
-                </View>
+                    <View>
+                        <Title>Faça seu logon</Title>
+                    </View>
 
-                <Input name="email" icon="mail" placeholder="E-mail" />
-                <Input name="password" icon="lock" placeholder="Senha" />
+                    <Input name="email" icon="mail" placeholder="E-mail" />
+                    <Input name="password" icon="lock" placeholder="Senha" />
 
-                <Button>Entrar</Button>
+                    <Button>Entrar</Button>
 
-                <ForgotPassword>
-                    <ForgotPasswordText>
-                        Esqueci minha senha
-                    </ForgotPasswordText>
-                </ForgotPassword>
+                    <ForgotPassword>
+                        <ForgotPasswordText>
+                            Esqueci minha senha
+                        </ForgotPasswordText>
+                    </ForgotPassword>
 
-            </Container>
-
+                </Container>
+            </KeyboardAvoidingView>
             <NewAccount>
                 <Icon color="#ff9000" size={20} name="log-in" />
                 <NewAccountText>
