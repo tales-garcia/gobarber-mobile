a04d42eb648c1dc83942ad1c9d219545273619f2
Tales Garcia <garcia.tales@gmail.com>
2020-12-20 13:55:30 -0300

    feat(hooks): create auth context and hook
    Auth context stores the user and token

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,72 @@
+import React, { useCallback, useContext, useEffect, useState } from 'react';
+import { createContext } from "react";
+import storage from '@react-native-community/async-storage';
+import api from '../services/api';
+
+
+interface SignInCredentials {
+    email: string;
+    password: string;
+}
+
+interface AuthState {
+    token: string;
+    user: object;
+}
+
+interface AuthContextData {
+    user: object;
+    signIn(credentials: SignInCredentials): Promise<void>;
+    signOut(): void;
+}
+
+export const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+export function useAuth() {
+    const authContext = useContext(AuthContext);
+
+    if (!authContext) throw new Error('useAuth must be used within an AuthProvider');
+
+    return authContext;
+}
+
+export const AuthProvider: React.FC = ({ children }) => {
+    const [data, setData] = useState<AuthState>({} as AuthState);
+
+    useEffect(() => {
+        (async () => {
+            const [[, user], [, token]] = await storage.multiGet(['user', 'token']);
+
+            if (!user || !token) return {} as AuthState;
+
+            const parsedUser = JSON.parse(user) as object;
+
+            setData({ user: parsedUser, token });
+        })();
+    }, []);
+
+    const signIn = useCallback(async ({ email, password }: SignInCredentials) => {
+        const res = await api.post('sessions', {
+            email,
+            password
+        });
+
+        const { user, token } = res.data;
+
+        await storage.multiSet([['token', token], ['user', JSON.stringify(user)]]);
+
+        setData({ user, token });
+    }, []);
+
+    const signOut = useCallback(async () => {
+        await storage.multiRemove(['token', 'user']);
+
+        setData({} as AuthState);
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+            { children}
+        </AuthContext.Provider>
+    );
+}
