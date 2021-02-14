a5ff60c65cd51eb3e5a031784cde863c4c845ec2
Tales Garcia <garcia.tales@gmail.com>
2021-02-14 08:56:47 -0300

    feat(create appointment page): create header

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,10 +1,30 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { Container } from './styles';
+import { Icon } from 'react-native-vector-icons/Icon';
+import { useAuth } from '../../hooks/auth';
+import {
+    Container,
+    Header,
+    BackButton,
+    HeaderTitle,
+    Avatar
+} from './styles';
 
 const CreateAppointment: React.FC = () => {
+    const { user } = useAuth();
+    const { goBack } = useNavigation();
 
     return (
         <Container>
+            <Header>
+                <BackButton onPress={goBack}>
+                    <Icon name="chevron-left" size={24} color="#999591" />
+                </BackButton>
+
+                <HeaderTitle>Cabeleireiros</HeaderTitle>
+
+                <Avatar source={{ uri: user.avatarUrl }} />
+            </Header>
         </Container>
     );
 }
